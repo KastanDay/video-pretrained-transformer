8aa3fd6caa12e0f71d482eb4099e19846eb47c13
Kastan Day <kastanvday@gmail.com>
2022-11-07 17:07:05 +0000

    touched-up whisper video converting

--- data_preprocessing/whisper_audio/CaptionPreprocessing.py
+++ data_preprocessing/whisper_audio/CaptionPreprocessing.py
@@ -1,14 +1,13 @@
 
 from logging import raiseExceptions
 import os
+import sys
+import pathlib
 from os import path
 from lhotse import CutSet, RecordingSet, align_with_torchaudio, annotate_with_whisper
-# from tqdm import tqdm
-from pprint import pprint
 from dataclasses import asdict
-import torch
 from pydub import AudioSegment
-import sys
+import torch
 
 class CaptionPreprocessing:
     # Takes in a path to an mp4 file, converts it to wav
@@ -22,38 +21,32 @@ class CaptionPreprocessing:
         # self.cut = None
 
     # Converts mp4 to wav
-    def load_mp4_to_wav(self, path):
-        # convert my_file.mp4 to my_file.wav
-        # base_name =  os.path.splitext(path)[0]
-        # path_to_output_wav = base_name + '.wav'
+    def load_mp4_to_wav(self, video_path: str):
+        """convert my_file.mp4 to my_file.wav"""
         # command = "ffmpeg -i " + path + " -ab 160k -ac 1 -ar 16000 -vn " + path_to_output_wav
         # subprocess.call(command, shell=True)
-        # print("sucess")
         
         # todo: more reliable way to get codc
-        # p = Path(path_to_json)
-        # p = p.with_suffix('.npy')
+        out_path = pathlib.Path(video_path)
+        out_path = out_path.with_suffix('.wav')
         
-        base_name =  os.path.splitext(path)[0]
-        dst = base_name + '.wav'
+        sound = AudioSegment.from_file(path) # format="mp4" (not required)
+        sound.export(out_path, format="wav", parameters= ["-ac", "1"])
+        return out_path.as_posix()
 
-
-        # convert mp4 to wav
-        sound = AudioSegment.from_file(path,format="mp4") # todo: get codec from filename
-        sound.export(dst, format="wav", parameters= ["-ac", "1"])
-        return dst
-
-
-
-    # Input: segment time length, threshold of words/segment
-    # Output: list of dictionaries [{caption: string, start: int, end: int, [{word:, start:, end:,}, ...]}, ...]
-    # shape: segment, words in segment, {word: word, start: start, end: end}
-    # Example output: [{'caption': "THERE ARE NO STRANGERS TO LOVE YOU KNOW THE RULES AND SO DO I I'VE",
-    #  'start': 18.9245625, 'end': 27.3031875,
-    #  'segment_word_list': [{'word': 'THERE', 'start': 18.9245625, 'end': 19.0261875}, {'word': 'ARE', 'start': 19.0451875, 'end': 19.106562500000003}, {'word': 'NO', 'start': 19.125625, 'end': 19.307624999999998}, {'word': 'STRANGERS', 'start': 19.3466875, 'end': 19.769875000000003}, {'word': 'TO', 'start': 19.929625, 'end': 20.151812500000002}, {'word': 'LOVE', 'start': 20.3115, 'end': 20.4533125}, {'word': 'YOU', 'start': 22.82325, 'end': 22.965125}, {'word': 'KNOW', 'start': 23.004, 'end': 23.3466875}, {'word': 'THE', 'start': 23.3855, 'end': 23.5073125}, {'word': 'RULES', 'start': 23.606375, 'end': 24.1298125}, {'word': 'AND', 'start': 24.44975, 'end': 24.5515}, {'word': 'SO', 'start': 24.6505625, 'end': 24.912937499999998}, {'word': 'DO', 'start': 25.2128125, 'end': 25.49525}, {'word': 'I', 'start': 25.6545625, 'end': 25.676000000000002}, {'word': "I'VE", 'start': 27.1005, 'end': 27.3031875}]},
-    # ...]
 
     def get_segments_thresholded(self, time = 30, threshold = 15):
+        """  
+        Input: segment time length, threshold of words/segment
+        Output: list of dictionaries [{caption: string, start: int, end: int, [{word:, start:, end:,}, ...]}, ...]
+        Shape: segment, words in segment, {word: word, start: start, end: end}
+        Example output: 
+        [
+            {'caption': "THERE ARE NO STRANGERS TO LOVE YOU KNOW THE RULES AND SO DO I I'VE",
+            'start': 18.9245625, 'end': 27.3031875,
+            'segment_word_list': [{'word': 'THERE', 'start': 18.9245625, 'end': 19.0261875}, {'word': 'ARE', 'start': 19.0451875, 'end': 19.106562500000003}, {'word': 'NO', 'start': 19.125625, 'end': 19.307624999999998}, {'word': 'STRANGERS', 'start': 19.3466875, 'end': 19.769875000000003}, {'word': 'TO', 'start': 19.929625, 'end': 20.151812500000002}, {'word': 'LOVE', 'start': 20.3115, 'end': 20.4533125}, {'word': 'YOU', 'start': 22.82325, 'end': 22.965125}, {'word': 'KNOW', 'start': 23.004, 'end': 23.3466875}, {'word': 'THE', 'start': 23.3855, 'end': 23.5073125}, {'word': 'RULES', 'start': 23.606375, 'end': 24.1298125}, {'word': 'AND', 'start': 24.44975, 'end': 24.5515}, {'word': 'SO', 'start': 24.6505625, 'end': 24.912937499999998}, {'word': 'DO', 'start': 25.2128125, 'end': 25.49525}, {'word': 'I', 'start': 25.6545625, 'end': 25.676000000000002}, {'word': "I'VE", 'start': 27.1005, 'end': 27.3031875}]},
+        ]
+        """
         def get_cut(path): 
             success = False
             
@@ -75,7 +68,6 @@ class CaptionPreprocessing:
                 except ValueError or AssertionError:
                     print("ERROR... restarting")
 
-            
         def to_time_dict():
             time_dict_list = []
             supervision = "ERROR"
@@ -97,8 +89,8 @@ class CaptionPreprocessing:
                 # print("CUT", self.cut)
                 # print("SUPERVISION", supervision)
                 return
-
             return time_dict_list
+        
         if not self.cut:
             try:
                 self.cut = get_cut(self.mp3_path)
@@ -107,7 +99,6 @@ class CaptionPreprocessing:
                 fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
                 print(exc_type, fname, exc_tb.tb_lineno)
                 return
-
 
         time_dict_list = to_time_dict()
         curr_dict_list = []
