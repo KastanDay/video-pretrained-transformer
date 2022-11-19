393e5940c626e4de4718eaf3d9d1d6a84fbae81b
Kastan Day <kastanvday@gmail.com>
2022-11-19 23:35:03 +0000

    clean up old scene graph code

--- data_preprocessing/openpsg_inference/configs/_base_/models/psgtr_r101.py
+++
@@ -1,5 +0,0 @@
-_base_ = './psgtr_r50.py'
-
-model = dict(backbone=dict(
-    depth=101,
-    init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet101')))
--- data_preprocessing/openpsg_inference/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .utils import *  # noqa: F401,F403
-from .vis_tools import *  # noqa: F401,F403
-
