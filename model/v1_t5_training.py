e80dd37ae1f6963bd9eb0c9e30f8e169a332684e
Kastan Day <kastanvday@gmail.com>
2022-12-10 09:21:43 -0600

    iterative improvement of training loop

--- model/v1_t5_training.py
+++ model/v1_t5_training.py
@@ -4,29 +4,35 @@ import json
 import glob
 import traceback
 import numpy as np
-import pathlib
 from numpy import load
 import lovely_tensors as lt
 lt.monkey_patch()
 import tqdm
 import wandb
-wandb.init("custom-t5")
+import time
 
+# hyperparams 
+MODEL_VERSION_NAME  = 'yt_pretrain_adamW_iter99000'
 BATCH_NAME          = "parallel_15"
-MODEL_VERSION_NAME  = 'yt_pretrain'
+learning_rate       = 1e-4  # also good: 3e-4
+
 BASE_DIR            = '/scratch/bbki/kastanday/whisper'
 MODEL_SAVE_PATH     = f'{BASE_DIR}/MODEL_CHECKPOINTS/{MODEL_VERSION_NAME}'
 REMOTE_WHISPER_FILE = f'{BASE_DIR}/{BATCH_NAME}_whisper_output.jsonl'
 REMOTE_CLIP_DIR     = f'{BASE_DIR}/{BATCH_NAME}_clip_output'
 REMOTE_SCENE_FILE   = f'{BASE_DIR}/{BATCH_NAME}_scene_output.jsonl'
 
-# hyperparams 
-learning_rate = 1e-4
-
-wandb.config = {'learning_rate'     : learning_rate,
-                'batch_name'        : BATCH_NAME,
-                'model_save_path'   : MODEL_SAVE_PATH,
-                }
+wandb.init(
+    entity="kastan",
+    project="VPT-custom-t5",
+    name=MODEL_VERSION_NAME,
+    config={'learning_rate'     : learning_rate,
+            'batch_name'        : BATCH_NAME,
+            'model_save_path'   : MODEL_SAVE_PATH,
+            },
+    # group=datetime_str,
+    tags=['AdamW', ],
+)
 
 # Instantiate clip
 import clip
@@ -36,29 +42,25 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 print("Device: ", device)
 clip_instance, clip_preprocess = clip.load(MODEL_SIZE, device)
 
-'''
-T5 MODEL SELECTION
-
-T5 V1.1 --  https://huggingface.co/docs/transformers/model_doc/t5v1.1 && https://github.com/google-research/text-to-text-transfer-transformer/blob/main/released_checkpoints.md#t511
-small - base - large - 3b/xl - 11b/xxl
-
-OG: t5-small
-
-'google/t5-base-lm-adapt' # largest on my server (without float16)
-'google/t5-xl-lm-adapt'
-
-google/t5-v1_1-large
-'''
-
+# Instantiate CustomT5
 from transformers import T5Tokenizer, T5ForConditionalGeneration, T5Model, T5Config, AutoModelWithLMHead
-t5 = T5ForConditionalGeneration.from_pretrained(MODEL_SAVE_PATH, torch_dtype=torch.float32, low_cpu_mem_usage=False).to(device) # float16, True
+if os.path.exists(MODEL_SAVE_PATH):
+    # resume from checkpoint
+    # todo: NEED TO GET THE LATEST CHECPOINT...... 
+    print("🚨 MAJOR WARNING: Probably loading the WRONG CHECKPOINT!!! ")
+    # todo: use MODEL_SAVE_PATH and find the one with the highest iteration
+    t5 = T5ForConditionalGeneration.from_pretrained(f"{BASE_DIR}/MODEL_CHECKPOINTS/yt_pretrain_adamW_iter99000", torch_dtype=torch.float32, low_cpu_mem_usage=False).to(device) # float16, True
+    wandb.config.update({"starting_from_checkpoint": 0.1, "channels": 16})
+else:
+    t5 = T5ForConditionalGeneration.from_pretrained("google/t5-v1_1-base", torch_dtype=torch.float32, low_cpu_mem_usage=False).to(device) # float16, True
 t5_tokenizer = T5Tokenizer.from_pretrained("google/t5-v1_1-base", return_special_tokens_mask=True)
 # low_cpu_mem_usage(bool, optional) — Tries to not use more than 1x model size in CPU memory (including peak memory) while loading the model. experimental.
-optimizer = torch.optim.Adam(params =  t5.parameters(), lr=learning_rate) # Typically, 1e-4 and 3e-4 work well for most problems
+optimizer = torch.optim.AdamW(params =  t5.parameters(), lr=learning_rate) # Typically, 1e-4 and 3e-4 work well for most problems
 
 
 def log_gradient_norm():
     try: 
+        start_time = time.monotonic()
         total_norm = 0
         parameters = [p for p in t5.parameters() if p.grad is not None and p.requires_grad]
         for p in parameters:
@@ -66,6 +68,7 @@ def log_gradient_norm():
             total_norm += param_norm.item() ** 2
         total_norm = total_norm ** 0.5
         wandb.log({"total_gradient_norm": total_norm})
+        # print(f"Time to log norm: {(time.monotonic() - start_time):2f} seconds") # 0.01 seconds
         return total_norm
     except Exception as e:
         print("Failed to log gradient norm: ", e)
@@ -97,48 +100,46 @@ with jsonlines.open(REMOTE_SCENE_FILE, 'r') as scene_reader:
         # iterate over segments
         for segment_index in range(np_loaded['arr_0'].item()['total_segments']):
             try:
-                # print(np_loaded[f'arr_{segment_index}'].item()['captions'])
                 frame_embedding       = np_loaded[f'arr_{segment_index}'].item()['frame_embeddings']
                 caption_embedding     = np_loaded[f'arr_{segment_index}'].item()['text_caption_embeddings']
                 whisper_text_captions = np_loaded[f'arr_{segment_index}'].item()['captions']
-                
                 frame_embedding       = torch.from_numpy(frame_embedding.reshape((768,))).to(device)
                 caption_embedding     = torch.from_numpy(caption_embedding).to(device)
 
+                # embed whisper_text_captions with CLIP
                 scene_caption = scene_seg_list[segment_index]
                 scene_caption = clip.tokenize(scene_caption).to(device)
                 with torch.inference_mode(): # even faster than no_grad()
                     scene_embedding = clip_instance.encode_text(scene_caption)
                 scene_embedding = scene_embedding.reshape((768,))
-                
+
                 # Update embedding array
                 input_embeds_arr[0][0] = frame_embedding
                 input_embeds_arr[0][1] = caption_embedding
                 input_embeds_arr[0][2] = scene_embedding
                 # Set to torch
                 decoder_input_embeds_arr = np.random.rand( *one_input_shape )  # .astype(np.float16) # need fp32
-                decoder_input_embeds_arr = decoder_input_embeds_arr
                 
-                print("Input shapes:", scene_embedding, caption_embedding, frame_embedding)
-                
-                
+                # print("Input shapes:", scene_embedding, caption_embedding, frame_embedding)
                 ''' Forward pass '''
                 labels = t5_tokenizer(whisper_text_captions, return_tensors="pt").input_ids.to(device)
                 outputs = t5.forward(inputs_embeds=input_embeds_arr, attention_mask=attn_mask_arr, labels=labels)
                 loss = outputs[0]
-                wandb.log({"loss": loss})
                 
                 ''' Backwards pass '''
                 optimizer.zero_grad()
                 loss.sum().backward()
                 optimizer.step()
                 train_itr += 1
-                # save checkpoints
+                # logging
+                if train_itr % 50 == 0:
+                    wandb.log({"loss": loss})
+                    log_gradient_norm()
                 # save checkpoints
                 if train_itr % 1500 == 0:
-                    print("SAVING MODEL CHECKPOING TO: ", f"{MODEL_VERSION_NAME}_iter{train_itr}")
-                    MODEL_SAVE_PATH = f"{MODEL_VERSION_NAME}_iter{train_itr}"
-                    t5.save_pretrained(MODEL_SAVE_PATH)
+                    save_path = f"{MODEL_SAVE_PATH}_iter{train_itr}"
+                    t5.save_pretrained(save_path)
+                    print("SAVED MODEL CHECKPOINT TO: ", save_path)
             except Exception as e:
                 print("During training loop: ", e)
                 print(traceback.format_exc())
