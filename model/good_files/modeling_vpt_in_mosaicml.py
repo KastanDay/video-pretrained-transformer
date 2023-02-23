7cb99a359cdf617b6241653e8913d610c03dfe6c
Kastan Day <kastanvday@gmail.com>
2023-02-23 23:41:38 +0000

    reformat with 2 spaces, not 4

--- model/good_files/modeling_vpt_in_mosaicml.py
+++ model/good_files/modeling_vpt_in_mosaicml.py
@@ -13,79 +13,79 @@ from transformers import (AutoModelWithLMHead, T5ForConditionalGeneration, T5Tok
 
 
 class VPT_model(ComposerModel):
-    '''
+  '''
   Custom VPT implementaiton for MosaicML.
   https://docs.mosaicml.com/en/v0.11.1/trainer/using_the_trainer.html
   '''
 
-    def __init__(self, model_version_name: str = '', model_save_path: str = '', model_huggingface_name: str = "google/t5-v1_1-large"):
-        super().__init__()
+  def __init__(self, model_version_name: str = '', model_save_path: str = '', model_huggingface_name: str = "google/t5-v1_1-large"):
+    super().__init__()
 
-        self.model = T5ForConditionalGeneration.from_pretrained(model_huggingface_name)  #.to(self.device)
-        self.t5_tokenizer = T5Tokenizer.from_pretrained(model_huggingface_name, return_special_tokens_mask=True)
-        # self.model.train()  -- already done by compser I think.
+    self.model = T5ForConditionalGeneration.from_pretrained(model_huggingface_name)  #.to(self.device)
+    self.t5_tokenizer = T5Tokenizer.from_pretrained(model_huggingface_name, return_special_tokens_mask=True)
+    # self.model.train()  -- already done by compser I think.
 
-        # I got this vocab size from outputs.logits (it's rounded up from tokenizer.vocab_size)
-        self.train_cross_entropy = LanguageCrossEntropy(vocab_size=32128, ignore_index=-100)
-        self.val_cross_entropy = LanguageCrossEntropy(vocab_size=32128, ignore_index=-100)
+    # I got this vocab size from outputs.logits (it's rounded up from tokenizer.vocab_size)
+    self.train_cross_entropy = LanguageCrossEntropy(vocab_size=32128, ignore_index=-100)
+    self.val_cross_entropy = LanguageCrossEntropy(vocab_size=32128, ignore_index=-100)
 
-    def forward(self, batch):
-        input_embeds_arr = torch.cat([batch['clip_pooled_embedding'], batch['clip_last_hidden_states'], batch['caption_embedding']],
-                                     dim=1)  # concat along sequence dimension
-        return self.model.forward(inputs_embeds=input_embeds_arr, attention_mask=batch['attn_mask_arr'], labels=batch['labels'])
+  def forward(self, batch):
+    input_embeds_arr = torch.cat([batch['clip_pooled_embedding'], batch['clip_last_hidden_states'], batch['caption_embedding']],
+                                 dim=1)  # concat along sequence dimension
+    return self.model.forward(inputs_embeds=input_embeds_arr, attention_mask=batch['attn_mask_arr'], labels=batch['labels'])
 
-    # todo: UNTESTED
-    def eval_forward(self, batch, outputs=None):
-        '''
+  # todo: UNTESTED
+  def eval_forward(self, batch, outputs=None):
+    '''
     Docs: https://docs.mosaicml.com/en/v0.11.1/api_reference/generated/composer.ComposerModel.html#composer.ComposerModel.eval_forward
     todo: Placeholder so that training doesn't crash.
     '''
-        ground_truth_labels = batch['labels'][0][batch['labels'][0] != -100]
-        num_new_tokens = ground_truth_labels.shape[0]
+    ground_truth_labels = batch['labels'][0][batch['labels'][0] != -100]
+    num_new_tokens = ground_truth_labels.shape[0]
 
-        input_embeds_arr = torch.cat([batch['clip_pooled_embedding'], batch['clip_last_hidden_states'], batch['caption_embedding']],
-                                     dim=1)  # concat along sequence dimension
+    input_embeds_arr = torch.cat([batch['clip_pooled_embedding'], batch['clip_last_hidden_states'], batch['caption_embedding']],
+                                 dim=1)  # concat along sequence dimension
 
-        self.model.eval()
-        with torch.no_grad():
-            outputs = self.model.forward(inputs_embeds=input_embeds_arr,
-                                         attention_mask=batch['attn_mask_arr'],
-                                         labels=batch['labels'],
-                                         return_dict=True)
+    self.model.eval()
+    with torch.no_grad():
+      outputs = self.model.forward(inputs_embeds=input_embeds_arr,
+                                   attention_mask=batch['attn_mask_arr'],
+                                   labels=batch['labels'],
+                                   return_dict=True)
 
-        # generated vs actual tokens.
-        # todo: jus tpass logits and labels, no edits at all. Works cuz I already set Ignore index = -100, so they match.
-        self.val_cross_entropy.update(outputs.logits[0][0:num_new_tokens], batch['labels'][0][0:num_new_tokens])
+    # generated vs actual tokens.
+    # todo: jus tpass logits and labels, no edits at all. Works cuz I already set Ignore index = -100, so they match.
+    self.val_cross_entropy.update(outputs.logits[0][0:num_new_tokens], batch['labels'][0][0:num_new_tokens])
 
-        print("Cross entropy: ", self.val_cross_entropy.compute())
-        wandb.log({"val_loss_cross_entropy": self.val_cross_entropy.compute()})
+    print("Cross entropy: ", self.val_cross_entropy.compute())
+    wandb.log({"val_loss_cross_entropy": self.val_cross_entropy.compute()})
 
-        # batch['labels'][0][0:num_new_tokens]
+    # batch['labels'][0][0:num_new_tokens]
 
-        return outputs
+    return outputs
 
-    def update_metric(self, batch: Any, outputs: Any, metric: Metric) -> None:
-        return metric.update(outputs.logits, batch['labels'])
+  def update_metric(self, batch: Any, outputs: Any, metric: Metric) -> None:
+    return metric.update(outputs.logits, batch['labels'])
 
-    # todo: UNTESTED
-    def get_metrics(self, is_train: bool) -> Dict[str, Metric]:
-        if is_train:
-            return {"loss": self.train_cross_entropy}
-        else:
-            return {"val_loss": self.val_cross_entropy}
+  # todo: UNTESTED
+  def get_metrics(self, is_train: bool) -> Dict[str, Metric]:
+    if is_train:
+      return {"loss": self.train_cross_entropy}
+    else:
+      return {"val_loss": self.val_cross_entropy}
 
-    def loss(self, outputs, batch):
-        '''
+  def loss(self, outputs, batch):
+    '''
         Return loss from huggingface model outputs.
         Docs'''
-        loss = outputs[0].sum()
-        wandb.log({"train_loss": loss})
-        return loss
+    loss = outputs[0].sum()
+    wandb.log({"train_loss": loss})
+    return loss
 
-    # todo: not necessary?
-    # def metrics(self, train: bool = False):
-    #     return self.train_cross_entropy if train else self.val_cross_entropy
-    # MetricCollection([self.val_acc, self.val_loss]) <-- for multiple metrics
+  # todo: not necessary?
+  # def metrics(self, train: bool = False):
+  #     return self.train_cross_entropy if train else self.val_cross_entropy
+  # MetricCollection([self.val_acc, self.val_loss]) <-- for multiple metrics
 
 
 '''
@@ -114,7 +114,7 @@ t5_tokenizer = T5Tokenizer.from_pretrained(model_huggingface_name, return_specia
 
 
 def vpt_transform_dataset_to_batch(segment_batch):
-    '''
+  '''
   param: segment_batch: IterableOrderedDict. 1 SEGMENT (not batch_size, just one).
   The dataloader controls batch size, this just returns a single batch.
   
@@ -128,67 +128,67 @@ def vpt_transform_dataset_to_batch(segment_batch):
   returns: batch dictionary.  Keys: input_embeds_arr, attn_mask_arr, labels_tokenized
                               Values: batched Torch Tensors of shape <1, 1024, 1024>. These are stacked to create a batch.
   '''
-    # todo: remove all mention of cuda from this functino (no .todevice), then use pin=True.
-    # device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-    # print("👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉 SEGMENT BATCH", flush=True)
-    batch = {}  # keys: input_embeds_arr, attn_mask_arr, labels
+  # todo: remove all mention of cuda from this functino (no .todevice), then use pin=True.
+  # device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+  # print("👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉 SEGMENT BATCH", flush=True)
+  batch = {}  # keys: input_embeds_arr, attn_mask_arr, labels
 
-    # maybe don't loop here??? just do it once. Not sure how it works on ds.batch_size setting.
-    # Loop over BATCH_SIZE. Create dictionary where key = name, value = batched tensor
-    for key, numpy_array in zip(segment_batch.keys(), segment_batch):
-        # print("------------- PRINTING SEGMENT --------- ")
-        if key == 'clip_pooled_embedding':
-            # print("⭐️1️⃣ pooled embedding")
-            numpy_array = numpy_array.reshape(1, -1)  # for batch size purposes.
-            if key in batch.keys():
-                batch[key] = torch.cat((batch[key], torch.from_numpy(numpy_array)), dim=0)
-            else:
-                batch[key] = torch.from_numpy(numpy_array)
+  # maybe don't loop here??? just do it once. Not sure how it works on ds.batch_size setting.
+  # Loop over BATCH_SIZE. Create dictionary where key = name, value = batched tensor
+  for key, numpy_array in zip(segment_batch.keys(), segment_batch):
+    # print("------------- PRINTING SEGMENT --------- ")
+    if key == 'clip_pooled_embedding':
+      # print("⭐️1️⃣ pooled embedding")
+      numpy_array = numpy_array.reshape(1, -1)  # for batch size purposes.
+      if key in batch.keys():
+        batch[key] = torch.cat((batch[key], torch.from_numpy(numpy_array)), dim=0)
+      else:
+        batch[key] = torch.from_numpy(numpy_array)
 
-        elif key == 'caption_embedding':
-            # print("⭐️2️⃣ caption embedding")
-            # keep only the first HALF of caption embedding.
-            caption_length = numpy_array.shape[0]
-            s_half = caption_length // 2
-            # constant length of 446, pad with zeros. 446 is the max length of a caption (1024 - 577 - 1).
-            caption_embedding_full_length = torch.zeros((446, 1024))
-            caption_embedding_full_length[0:s_half] = torch.from_numpy(numpy_array[0:s_half])
+    elif key == 'caption_embedding':
+      # print("⭐️2️⃣ caption embedding")
+      # keep only the first HALF of caption embedding.
+      caption_length = numpy_array.shape[0]
+      s_half = caption_length // 2
+      # constant length of 446, pad with zeros. 446 is the max length of a caption (1024 - 577 - 1).
+      caption_embedding_full_length = torch.zeros((446, 1024))
+      caption_embedding_full_length[0:s_half] = torch.from_numpy(numpy_array[0:s_half])
 
-            # setup attention mask now that we know full length of caption
-            att_mask_shape = [1024]
-            attn_mask_arr = torch.zeros(att_mask_shape)
-            attn_mask_arr[0:578 + s_half] = 1
+      # setup attention mask now that we know full length of caption
+      att_mask_shape = [1024]
+      attn_mask_arr = torch.zeros(att_mask_shape)
+      attn_mask_arr[0:578 + s_half] = 1
 
-            if key in batch.keys():
-                # batch[key] = torch.cat((batch[key], torch.ones(10)), dim=0) # todo BAD for testing
-                batch[key] = torch.cat((batch[key], caption_embedding_full_length), dim=0)
-                batch['attn_mask_arr'] = torch.cat((batch['attn_mask_arr'], attn_mask_arr), dim=0)
-            else:
-                # batch[key] = torch.ones(10)  # todo BAD for testing
-                batch[key] = caption_embedding_full_length
-                batch['attn_mask_arr'] = attn_mask_arr
+      if key in batch.keys():
+        # batch[key] = torch.cat((batch[key], torch.ones(10)), dim=0) # todo BAD for testing
+        batch[key] = torch.cat((batch[key], caption_embedding_full_length), dim=0)
+        batch['attn_mask_arr'] = torch.cat((batch['attn_mask_arr'], attn_mask_arr), dim=0)
+      else:
+        # batch[key] = torch.ones(10)  # todo BAD for testing
+        batch[key] = caption_embedding_full_length
+        batch['attn_mask_arr'] = attn_mask_arr
 
-        elif key == 'clip_last_hidden_states':
-            # print("⭐️3️⃣ clip last hidden states")
-            if key in batch.keys():
-                batch[key] = torch.cat((batch[key], torch.from_numpy(numpy_array)), dim=0)
-            else:
-                batch[key] = torch.from_numpy(numpy_array)
+    elif key == 'clip_last_hidden_states':
+      # print("⭐️3️⃣ clip last hidden states")
+      if key in batch.keys():
+        batch[key] = torch.cat((batch[key], torch.from_numpy(numpy_array)), dim=0)
+      else:
+        batch[key] = torch.from_numpy(numpy_array)
 
-        # todo; clean this up IDK what exactly the labels look like.
-        elif key == 'caption':
-            caption = numpy_array[0]  # passed in as a single-element list.
-            # print("⭐️4️⃣ CAPTION")
-            # print(caption)
-            full_caption_tokenized = t5_tokenizer(caption, padding=False, truncation=True, return_tensors="pt").input_ids
-            caption_length = full_caption_tokenized.shape[1]
-            s_half = caption_length // 2
-            # only keep 2nd half of caption to use as labels.
-            proper_shape = full_caption_tokenized[0][s_half:].shape[0]
-            labels_full_length = torch.ones((512), dtype=torch.int64) * -100
-            labels_full_length[:proper_shape] = full_caption_tokenized[0][s_half:]  # 👈 take 2nd half!!
-            if 'labels' in batch.keys():
-                batch['labels'] = torch.cat((batch['labels'], labels_full_length), dim=0)
-            else:
-                batch['labels'] = labels_full_length
-    return batch
+    # todo; clean this up IDK what exactly the labels look like.
+    elif key == 'caption':
+      caption = numpy_array[0]  # passed in as a single-element list.
+      # print("⭐️4️⃣ CAPTION")
+      # print(caption)
+      full_caption_tokenized = t5_tokenizer(caption, padding=False, truncation=True, return_tensors="pt").input_ids
+      caption_length = full_caption_tokenized.shape[1]
+      s_half = caption_length // 2
+      # only keep 2nd half of caption to use as labels.
+      proper_shape = full_caption_tokenized[0][s_half:].shape[0]
+      labels_full_length = torch.ones((512), dtype=torch.int64) * -100
+      labels_full_length[:proper_shape] = full_caption_tokenized[0][s_half:]  # 👈 take 2nd half!!
+      if 'labels' in batch.keys():
+        batch['labels'] = torch.cat((batch['labels'], labels_full_length), dim=0)
+      else:
+        batch['labels'] = labels_full_length
+  return batch
--- model/good_files/v4_two_half_mosaic.py
+++ model/good_files/v4_two_half_mosaic.py
@@ -44,22 +44,22 @@ result = subprocess.run(["hostname"], capture_output=True, text=True)
 hostname = str(result.stdout.strip())
 
 if "gpu" in hostname or "gpu" in hostname:
-    print("Hostname: Delta GPU")
-    BASE_DIR = '/scratch/bbki/kastanday/whisper'
+  print("Hostname: Delta GPU")
+  BASE_DIR = '/scratch/bbki/kastanday/whisper'
 elif 'storage' in hostname:
-    print("Hostname: kastan storage")
-    BASE_DIR = '/mnt/storage_ssd'
+  print("Hostname: kastan storage")
+  BASE_DIR = '/mnt/storage_ssd'
 elif 'dgx' in hostname:
-    print("Hostname: DGX")
-    BASE_DIR = '/raid/projects/kastan'
+  print("Hostname: DGX")
+  BASE_DIR = '/raid/projects/kastan'
 elif 'hal' in hostname:
-    print("Hostname: HAL")
-    BASE_DIR = '~/thesis/VPT_data/'
+  print("Hostname: HAL")
+  BASE_DIR = '~/thesis/VPT_data/'
 elif '103ai' in hostname:
-    print("Hostname: 103ai, EPYC")
-    # BASE_DIR = '/mnt/teton/vpt/data/'
-    # BASE_DIR = '/mnt/teton/vpt/data/deeplake_parallel_15/'
-    BASE_DIR = '/mnt/teton/vpt/data/deeplake_handpicked/'
+  print("Hostname: 103ai, EPYC")
+  # BASE_DIR = '/mnt/teton/vpt/data/'
+  # BASE_DIR = '/mnt/teton/vpt/data/deeplake_parallel_15/'
+  BASE_DIR = '/mnt/teton/vpt/data/deeplake_handpicked/'
 
 # MANUAL PARAMS
 learning_rate = 1e-4  # recc for t5: 1e-4 and 3e-4
@@ -74,137 +74,137 @@ DATABASE_FILEPATH = f'{BASE_DIR}/CLIP_encode_results_{BATCH_NAME}'
 
 
 def main():
-    # create dataloader
-    ds = dl.load(DATABASE_FILEPATH)
-    columns_for_training = ['clip_pooled_embedding', 'caption_embedding', 'clip_last_hidden_states', 'caption']
+  # create dataloader
+  ds = dl.load(DATABASE_FILEPATH)
+  columns_for_training = ['clip_pooled_embedding', 'caption_embedding', 'clip_last_hidden_states', 'caption']
 
-    # todo: remove all mention of cuda from this functino (no .todevice), then use pin=True.
-    train_dataloader = ds.pytorch(
-        tensors=columns_for_training,
-        transform=vpt_transform_dataset_to_batch,
-        num_workers=psutil.cpu_count(),  # why can't I change this... 
-        batch_size=batch_size,
-        pin_memory=True,
-        shuffle=False,
-        drop_last=False,
-        use_local_cache=False,  # downloads to ~/.deeplake
-    )
+  # todo: remove all mention of cuda from this functino (no .todevice), then use pin=True.
+  train_dataloader = ds.pytorch(
+      tensors=columns_for_training,
+      transform=vpt_transform_dataset_to_batch,
+      num_workers=psutil.cpu_count(),
+      batch_size=batch_size,
+      pin_memory=True,
+      shuffle=False,
+      drop_last=False,
+      use_local_cache=False,  # downloads to ~/.deeplake, good when using S3.
+  )
 
-    # todo: use c++ dataloader "deeplake[enterprise]"
-    # train_loader = ds.dataloader()\
-    #              .transform(transform)\
-    #              .batch(32)\
-    #              .shuffle(True)\
-    #              .pytorch(tensors=['images', 'labels'], num_workers = 8)
+  # todo: use c++ dataloader "deeplake[enterprise]"
+  # train_loader = ds.dataloader()\
+  #              .transform(transform)\
+  #              .batch(32)\
+  #              .shuffle(True)\
+  #              .pytorch(tensors=['images', 'labels'], num_workers = 8)
 
-    # run training with our model
-    # todo: implement evaluation or something on a holdout/validation set. Maybe yt1b val.
-    model = VPT_model(model_huggingface_name=model_huggingface_name, model_version_name=MODEL_VERSION_NAME)
+  # run training with our model
+  # todo: implement evaluation or something on a holdout/validation set. Maybe yt1b val.
+  model = VPT_model(model_huggingface_name=model_huggingface_name, model_version_name=MODEL_VERSION_NAME)
 
-    # adafactor setup as suggested here: https://discuss.huggingface.co/t/t5-finetuning-tips/684/3
-    # todo: Critically implement LR warmup if using adafactor.
-    # Training without LR warmup or clip_threshold is not recommended.
-    # use scheduled LR warm-up to fixed LR
-    # FOR EX:
-    # from transformers.optimization import Adafactor, AdafactorSchedule
-    # optimizer = Adafactor(model.parameters(), scale_parameter=True, relative_step=True, warmup_init=True, lr=None)
-    # lr_scheduler = AdafactorSchedule(optimizer)
-    # optimizer = transformers.Adafactor(params=model.parameters(), lr=0.001, scale_parameter=False, relative_step=False)
-    # fsdp_config['min_params']
+  # adafactor setup as suggested here: https://discuss.huggingface.co/t/t5-finetuning-tips/684/3
+  # todo: Critically implement LR warmup if using adafactor.
+  # Training without LR warmup or clip_threshold is not recommended.
+  # use scheduled LR warm-up to fixed LR
+  # FOR EX:
+  # from transformers.optimization import Adafactor, AdafactorSchedule
+  # optimizer = Adafactor(model.parameters(), scale_parameter=True, relative_step=True, warmup_init=True, lr=None)
+  # lr_scheduler = AdafactorSchedule(optimizer)
+  # optimizer = transformers.Adafactor(params=model.parameters(), lr=0.001, scale_parameter=False, relative_step=False)
+  # fsdp_config['min_params']
 
-    # all params are fp32
-    # for param in model.parameters():
-    #   print(param.dtype)
+  # all params are fp32
+  # for param in model.parameters():
+  #   print(param.dtype)
 
-    optimizer = torch.optim.AdamW(params=model.parameters(), lr=learning_rate)  # Typically, 1e-4 and 3e-4 work well for most problems
-    wandb_logger = WandBLogger(
-        init_kwargs={
-            "config": {
-                'learning_rate': learning_rate,
-                'batch_name': BATCH_NAME,
-                'dataset_path': DATABASE_FILEPATH,
-                'model_save_path': MODEL_SAVE_PATH,
-            },
-            "entity": "kastan",
-            "project": "VPT-custom-t5",
-            "name": MODEL_VERSION_NAME,
-            # group=datetime_str,
-            "tags": [
-                # 'Adafactor w/ auto_lr',
-                'Adamw',
-                # 'FSDP',
-                'MosaicML',
-                'resume_from_checkpoint',
-            ],
-        })
+  optimizer = torch.optim.AdamW(params=model.parameters(), lr=learning_rate)  # Typically, 1e-4 and 3e-4 work well for most problems
+  wandb_logger = WandBLogger(
+      init_kwargs={
+          "config": {
+              'learning_rate': learning_rate,
+              'batch_name': BATCH_NAME,
+              'dataset_path': DATABASE_FILEPATH,
+              'model_save_path': MODEL_SAVE_PATH,
+          },
+          "entity": "kastan",
+          "project": "VPT-custom-t5",
+          "name": MODEL_VERSION_NAME,
+          # group=datetime_str,
+          "tags": [
+              # 'Adafactor w/ auto_lr',
+              'Adamw',
+              # 'FSDP',
+              'MosaicML',
+              'resume_from_checkpoint',
+          ],
+      })
 
-    ## FSDP config: https://docs.mosaicml.com/en/v0.11.1/notes/distributed_training.html#composer-s-fsdp-auto-wrap-policy
-    # If any module has more parameters than fsdp_config['min_params'], it will be wrapped.
+  ## FSDP config: https://docs.mosaicml.com/en/v0.11.1/notes/distributed_training.html#composer-s-fsdp-auto-wrap-policy
+  # If any module has more parameters than fsdp_config['min_params'], it will be wrapped.
 
-    fsdp_config = {
-        'sharding_strategy': 'FULL_SHARD',
-        'min_params': 1e8,
-        'mixed_precision': 'DEFAULT',
-        'backward_prefetch': 'BACKWARD_POST',
-        'activation_checkpointing': False,
-        'verbose': True
-    }
+  fsdp_config = {
+      'sharding_strategy': 'FULL_SHARD',
+      'min_params': 1e8,
+      'mixed_precision': 'DEFAULT',
+      'backward_prefetch': 'BACKWARD_POST',
+      'activation_checkpointing': False,
+      'verbose': True
+  }
 
-    cosine_lr_schedule = optim.CosineAnnealingWithWarmupScheduler(t_warmup=Time.from_batch(2000))
+  cosine_lr_schedule = optim.CosineAnnealingWithWarmupScheduler(t_warmup=Time.from_batch(2000))
 
-    # alibi attention https://docs.mosaicml.com/en/v0.11.1/method_cards/alibi.html
-    # only directly supported for GPT and BERT.
-    # alibi = Alibi(
-    #     max_sequence_length=1024,
-    #     train_sequence_length_scaling=0.25,
-    # )
+  # alibi attention https://docs.mosaicml.com/en/v0.11.1/method_cards/alibi.html
+  # only directly supported for GPT and BERT.
+  # alibi = Alibi(
+  #     max_sequence_length=1024,
+  #     train_sequence_length_scaling=0.25,
+  # )
 
-    # Instantiate the trainer
-    composer_trace_dir = 'composer_profiler'
-    torch_trace_dir = 'torch_profiler'
+  # Instantiate the trainer
+  composer_trace_dir = 'composer_profiler'
+  torch_trace_dir = 'torch_profiler'
 
-    # trainer_device = 'cpu'
-    trainer_device = 'gpu'
-    print(f"Running Trainer on {trainer_device}")
-    trainer = Trainer(
-        model=model,
-        train_dataloader=train_dataloader,
-        eval_dataloader=train_dataloader,
-        optimizers=optimizer,
-        max_duration=3,  # epochs
-        device=trainer_device,  # "gpu" if torch.cuda.is_available() else "cpu",
-        run_name=f'{MODEL_VERSION_NAME}',
-        save_folder=f"{BASE_DIR}/{MODEL_VERSION_NAME}/checkpoints",
-        save_interval="1000ba",  # 2k batches
-        save_num_checkpoints_to_keep=2,
-        schedulers=[cosine_lr_schedule],
-        # algorithms=[alibi],  # FusedLayerNorm() -- use NGC
-        loggers=[wandb_logger],
-        # grad_accum=10, # requires multiple GPUs I guess
-        device_train_microbatch_size='auto',
-        # eval_interval=0,
-        precision='amp_bf16',  # working: fp32
-        # fsdp_config=fsdp_config,
-        # save_folder="s3://my-bucket/{run_name}/checkpoints",
-        # save_filename="ep{epoch}.pt",
-        save_overwrite=True,
-        # load_path=f"{MODEL_SAVE_PATH}/latest-rank0.pt",  # resume from checkpoint
-        # "/raid/projects/kastan/mosaic_yt_pretrain_half_half/checkpoints/latest-rank0.pt",  # resume from checkpoint
-        # train_subset_num_batches=16,
-        # profiler=Profiler(
-        #     trace_handlers=[JSONTraceHandler(folder=composer_trace_dir, overwrite=True)],
-        #     schedule=cyclic_schedule(
-        #         wait=0,
-        #         warmup=1,
-        #         active=4,
-        #         repeat=1,
-        #     ),
-        #     torch_prof_folder=torch_trace_dir,
-        #     torch_prof_overwrite=True,
-        # ),
-        seed=42)
-    trainer.fit()
+  # trainer_device = 'cpu'
+  trainer_device = 'gpu'
+  print(f"Running Trainer on {trainer_device}")
+  trainer = Trainer(
+      model=model,
+      train_dataloader=train_dataloader,
+      eval_dataloader=train_dataloader,
+      optimizers=optimizer,
+      max_duration=3,  # epochs
+      device=trainer_device,  # "gpu" if torch.cuda.is_available() else "cpu",
+      run_name=f'{MODEL_VERSION_NAME}',
+      save_folder=f"{BASE_DIR}/{MODEL_VERSION_NAME}/checkpoints",
+      save_interval="1000ba",  # 2k batches
+      save_num_checkpoints_to_keep=2,
+      schedulers=[cosine_lr_schedule],
+      # algorithms=[alibi],  # FusedLayerNorm() -- use NGC
+      loggers=[wandb_logger],
+      # grad_accum=10, # requires multiple GPUs I guess
+      device_train_microbatch_size='auto',
+      # eval_interval=0,
+      precision='amp_bf16',  # working: fp32
+      # fsdp_config=fsdp_config,
+      # save_folder="s3://my-bucket/{run_name}/checkpoints",
+      # save_filename="ep{epoch}.pt",
+      save_overwrite=True,
+      # load_path=f"{MODEL_SAVE_PATH}/latest-rank0.pt",  # resume from checkpoint
+      # "/raid/projects/kastan/mosaic_yt_pretrain_half_half/checkpoints/latest-rank0.pt",  # resume from checkpoint
+      # train_subset_num_batches=16,
+      # profiler=Profiler(
+      #     trace_handlers=[JSONTraceHandler(folder=composer_trace_dir, overwrite=True)],
+      #     schedule=cyclic_schedule(
+      #         wait=0,
+      #         warmup=1,
+      #         active=4,
+      #         repeat=1,
+      #     ),
+      #     torch_prof_folder=torch_trace_dir,
+      #     torch_prof_overwrite=True,
+      # ),
+      seed=42)
+  trainer.fit()
 
 
 if __name__ == "__main__":
-    main()
+  main()
