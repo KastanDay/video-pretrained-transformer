fb7f2821fb6294678f41a7fc86e93007da204801
Kastan Day <kastanvday@gmail.com>
2023-03-21 23:45:21 +0000

    TVQA eval, fixing filepaths parsing

--- model/good_files/TVQA_eval.py
+++ model/good_files/TVQA_eval.py
@@ -1,5 +1,6 @@
 import json
 import os
+import pathlib
 from typing import Dict, List
 
 import numpy as np
@@ -23,14 +24,30 @@ class TVQA_eval():
       self.subtitles = [json.loads(line) for line in f]
     # len(subtitles)  # 21,793
 
-    # load model
-    from transformers import AutoTokenizer, T5ForConditionalGeneration
+    # from transformers import AutoTokenizer, T5ForConditionalGeneration
 
-    self.device = 'cuda:0'
+    # self.device = 'cuda:0'
+    # model_name = 'google/flan-t5-xl'
+    # self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+    # self.model = T5ForConditionalGeneration.from_pretrained(model_name).to(self.device)
 
-    model_name = 'google/flan-t5-xl'
-    self.tokenizer = AutoTokenizer.from_pretrained(model_name)
-    self.model = T5ForConditionalGeneration.from_pretrained(model_name).to(self.device)
+    self.tvqa_train_to_path = {
+        "House M.D.": 'house_frames',
+        "The Big Bang Theory": 'bbt_frames',
+        "Castle": 'castle_frames',
+        "How I Met You Mother": 'met_frames',
+        "Grey's Anatomy": 'grey_frames',
+        "Friends": 'friends_frames',
+    }
+
+    self.vid_name_prefix_to_path = {
+        "house": 'house_frames',
+        "castle": 'castle_frames',
+        "met": 'met_frames',
+        "grey": 'grey_frames',
+        "friends": 'friends_frames',
+        "": 'bbt_frames',  # no prefix at all used for bbt, it's the "default"
+    }
 
   def get_subtitle_from_clip(self, vid_name: str, start_time: float, end_time: float):
     subtitle = ''
@@ -56,24 +73,37 @@ class TVQA_eval():
         for ans_candidate in [qa['a0'], qa['a1'], qa['a2'], qa['a3'], qa['a4']]
     ]
 
-  def vid_name_to_path(self, show_name, vid_name):
-    tvqa_train_to_path = {
-        "House M.D.": 'house_frames',
-        "The Big Bang Theory": 'bbt_frames',
-        "Castle": 'castle_frames',
-        "How I Met You Mother": 'met_frames',
-        "Grey's Anatomy": 'grey_frames',
-        "Friends": 'friends_frames',
-    }
+  def vid_name_to_frames_path(self, vid_name):
+    '''
+    # house_s02e05_seg02_clip_11
+    # show_name_path = house_frames
+    # clip_name_path = s02e05_seg02_clip_11
+    '''
 
-    frames_dir = os.path.join('/teton/vpt/data/benchmark_datasets/TVQA/uncompressed_frames/frames_hq', tvqa_train_to_path['show_name'],
-                              vid_name)
+    show_name, clip_name_path = vid_name.split('_', 1)[0], vid_name.split('_', 1)[1]
+    if show_name not in self.vid_name_prefix_to_path:
+      show_name_path = 'bbt_frames'
+      clip_name_path = vid_name  # no prefix at all used for bbt. vid_name is actually the 'clip_name_path'
+    else:
+      show_name_path = self.vid_name_prefix_to_path[show_name]
+
+    print(show_name_path, clip_name_path)
+
+    frames_dir = os.path.join('/mnt/teton/vpt/data/benchmark_datasets/TVQA/uncompressed_frames/frames_hq', show_name_path, clip_name_path)
+
     # todo need to pick some subset of frames to use inside this folder.
     # get all files in this folder
     frames = os.listdir(frames_dir)
-    return frames
+    return frames_dir
 
   def get_clip_embed_from_vid_name(self, vid_name, start_time, end_time):
+    base_frames_filepath = pathlib.Path("/teton/vpt/data/benchmark_datasets/TVQA/uncompressed_frames/frames_hq")
+    # collect all frames from filepath
+    pathlib.Path(base_frames_filepath / vid_name).glob('*.jpg')
+    # Castle avg frames/clip = 274.7973615917
+    # BBT avg frames/clip = 186.3853298404
+
+  def get_flant5_embed_from_vid_name(self, vid_name, start_time, end_time):
     base_frames_filepath = "/teton/vpt/data/benchmark_datasets/TVQA/uncompressed_frames/frames_hq"
     base_frames_filepath
 
